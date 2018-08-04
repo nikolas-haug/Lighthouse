929691d8af5f9e64cbcd6f3d1f30580209dec7de
Eric Baltutis <baltu008@umn.edu>
2018-08-04 13:36:13 -0500

    reactstrap entry.js

--- client/src/components/pages/Entry/Entry.js
+++ client/src/components/pages/Entry/Entry.js
@@ -1,24 +1,49 @@
 import React from 'react'
 import Textarea from 'react-textarea-autosize';
+import { Jumbotron, Container, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 import './entry.css'
 
-const Entry = (props) => {
+// const Entry = (props) => {
 
+//     return (
+//         <div>
+//             <form>
+//                 <div className="form-group">
+//                     <input type="text" className="form-control" id="entry-name" placeholder="Name this entry" />
+//                 </div>
+//                 <hr />
+//                 <div className="form-group">
+//                     <Textarea className="form-control" id="entry-body" placeholder='Write about your day...'/>
+//                 </div>
+//                 <button type="submit" className="btn btn-primary">Submit</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+const Entry = (props) => {
     return (
-        <div>
-            <form>
-                <div className="form-group">
-                    <input type="text" className="form-control" id="entry-name" placeholder="Name this entry" />
-                </div>
-                <hr />
-                <div className="form-group">
-                    <Textarea className="form-control" id="entry-body" placeholder='Write about your day...'/>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    )
-}
+      <div>
+        <Jumbotron fluid>
+          <Container fluid>
+          <Form>
+        <FormGroup className="form-group">
+          <Label>Entry Title</Label>
+          <Input type="text" className="form-control" id="entry-name" placeholder="Name this entry" />
+        </FormGroup>
+
+        <FormGroup className="form-group">
+          <Label>Journal Entry</Label>
+          <Input className="form-control" id="entry-body" placeholder='Write about your day...' />
+        </FormGroup>
+
+        <Button>Submit</Button>
+      </Form>
+          </Container>
+        </Jumbotron>
+      </div>
+    );
+  };
 
 export default Entry;
