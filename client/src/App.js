da1a613818c9a532c74f62430238811798f9ac1d
Ryan Helgerson <ryan.helgerson@gmail.com>
2018-08-04 11:49:35 -0500

    Renaming entry component and journal page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,19 @@
 import React, { Component } from 'react';
 import Entry from './components/Journal/Entry'
 import Wrapper from './components/Wrapper/Wrapper'
-<<<<<<< HEAD
-import Landing from './components/Landing/Landing'
-
-class App extends Component {
-  render() {
-    return (
-      <Wrapper>
-        <Landing />
-      </Wrapper>
-    );
-  }
-}
-=======
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/pages/Home";
-import Profile from "./components/pages/Profile";
+import Home from "./components/pages/Homge";
+import Journal from "./components/pages/Journal";
 import Services from "./components/pages/Services";
 
 const App = () => (
   <Router>
     <div>
       <Route exact path="/" component={Home} />
-      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/journal" component={Journal} />
       <Route exact path="/services" component={Services} />
     </div>
   </Router>
 );
->>>>>>> helgy
 
 export default App;
