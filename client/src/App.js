c1585257d1eedff904a850e0478c49ab9cc27028
Ryan Helgerson <ryan.helgerson@gmail.com>
2018-08-04 11:35:12 -0500

    Adding react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Entry from './components/Journal/Entry'
 import Wrapper from './components/Wrapper/Wrapper'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Profile from "./components/pages/Profile";
+import Services from "./components/pages/Services";
 
-class App extends Component {
-  render() {
-    return (
-      <Wrapper>
-        <Entry />
-      </Wrapper>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/services" component={Services} />
+    </div>
+  </Router>
+);
 
 export default App;
