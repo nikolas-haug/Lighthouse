c02484af307b3251615d9ed7939221034285b0a4
Gavin  peterson <gavinpeterson@guest-10-166-8-88.wireless.umn.edu>
2018-08-04 12:04:49 -0500

    added entry

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import Entry from './components/Entry/Entry'
+import Entry from './components/pages/Entry/Entry'
 import Wrapper from './components/Wrapper/Wrapper'
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Journal from "./components/pages/Journal";
 import Services from "./components/pages/Services";
-import Login from './components/Login/Login'
+import Login from './components/pages/Login/Login'
 
 const App = () => (
   <Wrapper>
     <Router>
       <div>
         <Route exact path="/" component={Login} />
-        <Route exact path="/journal" component={Journal} />
+        <Route exact path="/journal" component={Entry} />
         <Route exact path="/services" component={Services} />
       </div>
     </Router>
