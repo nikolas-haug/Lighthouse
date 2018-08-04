ddab69c5dfd195e7f92b1a3396e3a764c002e655
Gavin  peterson <gavinpeterson@guest-10-166-8-88.wireless.umn.edu>
2018-08-04 10:16:28 -0500

    added landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Entry from './components/Journal/Entry'
 import Wrapper from './components/Wrapper/Wrapper'
+import Landing from './components/Landing/Landing'
 
 class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Entry />
+        <Landing />
       </Wrapper>
     );
   }
