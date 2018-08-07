e88e787341e4730856f3c8cc8813992bf4ecc5c2
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-06 19:23:03 -0500

    add lot out logic

--- client/src/components/pages/Login/Login.js
+++ client/src/components/pages/Login/Login.js
@@ -1,22 +1,42 @@
 import React from 'react'
+import { Component } from 'react';
 import { Link } from "react-router-dom";
+
 import About from '../Sections/About'
-import './login.css'
+import '../Login/login.css'
 
-const Login = (props) => {
-    // console.log(props)
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
 
-    return (
+        //handle the login form submission    
+        this.handleSubmit = event => {
+            //prevent that HTML behavor
+            event.preventDefault();
+            //Let reduce this to just data
+            let data = event.target;
+            //and create an object to hold user info
+            let user = {
+                username: data.username.value,
+                password: data.password.value
+            };;
+            //pass the user data over to this function we brought from landing component
+          this.props.handleUserLogin(user);
+          }
+    }
+    render() {
+        return(
         <div className="login-wrapper">
             <div className="login-block">
-                <form>
+                <form  onSubmit={this.handleSubmit}> 
                     <h3 className="login-title">Login</h3>
                     <div className="form-group">
-                        <input type="text" className="form-control" id="login-username" placeholder="Enter your username" />
+                        <input type="text" name="username" className="form-control" id="login-username" placeholder="Enter your username" />
                     </div>
                     <hr />
                     <div className="form-group">
-                        <input type="password" className="form-control" id="login-password" placeholder="Enter your password" />
+                        <input type="password" name="password" className="form-control" id="login-password" placeholder="Enter your password" />
                     </div>
                     <hr />
                     <button type="submit" className="btn btn-primary">Submit</button>
@@ -27,7 +47,7 @@ const Login = (props) => {
         </div>
     )
 
-
+}
 }
 
 export default Login;
