3e14b316bb8855ce62f2cc7cf689f1eeaa4a064e
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-06 19:20:27 -0500

    Add api routes

--- client/src/API/messenger.js
+++ client/src/API/messenger.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 
 export default {
-  // Send a new user data to the database
+  // Send a new user data to the express signup route
   sendNewUserInfo: function (newUser) {
     return axios.post('/signup', newUser);
   },
 
-  // Send a user login data to the database
-  // sendPreviousUserData: function (previousUser) {
-  //   return axios.post('/signin', previousUser);
-  // },
+  // Send a user login data to the express signin route 
+  sendPreviousUserData: function (user) {
+    return axios.post('/signin', user);
+  },
 };
