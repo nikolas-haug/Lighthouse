b47e8d713c58840fe3d30838a8a9ef58dd50427f
Eric Baltutis <baltu008@umn.edu>
2018-08-07 20:59:48 -0500

    fix collapsible navbar

--- client/src/components/Wrapper/Wrapper.js
+++ client/src/components/Wrapper/Wrapper.js
@@ -5,27 +5,57 @@ import "./wrapper.css";
 
 const Header = props => (
   <div>
-    <nav className="navbar navbar-dark bg-dark header">
-      <div className="logo">
-        <a href="/">
-          {" "}
-          <h1>LightHouse</h1>
-        </a>
-      </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark header">
+      <a class="navbar-brand" href="/">
+        LightHouse
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
 
-    <div className="header-btns">
-      <a>{localStorage.getItem("user")}</a>
-      <li><b className="fa fa-users" aria-hidden="true"></b><span><i>Forum</i></span></li>
-      <li><b className="fa fa-user" aria-hidden="true"></b><span><i>Journal</i></span></li>
-      <Link to={"/logout"} className="nav-link" aria-hidden="true">
-      <li><b className="fa fa-sign-out" aria-hidden="true"></b><span><i>Sign Out</i></span></li>
-      </Link>
-      <Link to={"/new_entry"} className="nav-link" aria-hidden="true">
-        Add Entry
-      </Link>
-      <Link to={"/entries"} className="nav-link" aria-hidden="true">
-        Entries
-      </Link>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li>
+            <b className="fa fa-user" aria-hidden="true" />
+            <span>
+              <i>Journal</i>
+            </span>
+          </li>
+          <a>{localStorage.getItem("user")}</a>
+          <li>
+            <b className="fa fa-users" aria-hidden="true" />
+            <span>
+              <i>Forum</i>
+            </span>
+          </li>
+          <Link to={"/new_entry"} className="nav-link" aria-hidden="true">
+            Add Entry
+          </Link>
+          <Link to={"/entries"} className="nav-link" aria-hidden="true">
+            Entries
+          </Link>
+        </ul>
+        <Link to={"/logout"} className="nav-link" aria-hidden="true">
+          <li>
+            <b className="fa fa-sign-out" aria-hidden="true" />
+            <span>
+              <i>Sign Out</i>
+            </span>
+          </li>
+        </Link>
       </div>
     </nav>
 
