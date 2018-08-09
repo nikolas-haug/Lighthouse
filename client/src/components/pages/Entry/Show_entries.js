cdf12fcd7322896683215add95f46b7fa99e1d55
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-09 13:20:25 -0500

    add some initial edit entry entry code

--- client/src/components/pages/Entry/Show_entries.js
+++ client/src/components/pages/Entry/Show_entries.js
@@ -4,6 +4,16 @@ import {
 } from 'react';
 
 class ShowEntries extends Component {
+    constructor(props) {
+        super(props);
+    this.handleAction = (e)=>{
+        e.preventDefault()
+        let entry_id = e.target.getAttribute('data-id')
+        let action = e.target.getAttribute('action')
+        action==='edit'? console.log("edit"):console.log("delete")
+        }        
+    }
+
     componentDidMount() {
         this.props.getAllEntries()
     }
@@ -12,7 +22,12 @@ class ShowEntries extends Component {
             <div>
                 <ul>
                     {this.props.entries.map(entry=>
-                        <li key={entry._id}>{entry.title}</li>
+                        <li key={entry._id}>{entry.title} 
+                            <button data-id={entry._id} action="edit"
+                                onClick={this.handleAction}>Edit</button> 
+                            <button data-id={entry._id} action="delete"
+                                onClick={this.handleAction}>Delete</button>
+                        </li>
                     )}
                 </ul>
             </div>
