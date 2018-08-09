1b69a94a0a0edd852f2e78e50c2a01dbabb0260f
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-09 09:27:50 -0500

    incoming

--- client/src/components/pages/Services/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button = (props) => {
-  return(
-    <button onClick={props.handleButtonClick} className="btn btn-warning btn-lg">Search for providers</button>
-  )
-};
-
-export default Button;
--- server/models/Comments.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require("mongoose"); 
-var CommentSchema = new mongoose.Schema({  
-    body: String,
-    likes: [],
-    }); 
-    module.exports = mongoose.model("Comment", CommentSchema);
