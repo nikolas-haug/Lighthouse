0f0b6506890e117d224f4f1db39796a11f44dab2
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-09 20:08:13 -0500

    add edit state

--- client/src/components/pages/Entry/Entry.js
+++ client/src/components/pages/Entry/Entry.js
@@ -10,7 +10,8 @@ class Entry extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            entries: []
+            entries: [],
+            editedEntry: {}
         };
 
         this.getAllEntries = () =>{
@@ -40,30 +41,23 @@ class Entry extends Component {
         }
 
 
-        // this.handleEditEntry = (user) => {
-        //     // Takes the submitted data and pass it over to the API module
-        //     API.sendPreviousUserData(user).then(res => {
-        //         //If sign in is success
-        //         if (res.data.username) {
-        //             //Store user info
-        //             localStorage.setItem('user', res.data.username)
-        //             localStorage.setItem('id', res.data._id)
-        //             // Set the state with the user info
-        //             this.setState({
-        //                 user: {
-        //                     id: res.data._id,
-        //                     username: localStorage.getItem('user')
-        //                 }
-        //             })
-        //             //Redirect to entry page for now
-        //             this.props.history.push('/entry');
-        //         } else {
-        //             //If sign in fails, stay on login
-        //             this.props.history.push('/');
-        //         }
-        //     }).catch(err => console.log(err));
+        this.handleEditEntry = (entry_id) => {
+            // Takes the submitted data and pass it over to the API module
+            API.sendEditedEntry(entry_id).then(res => {
+                //If sign in is success
+                if (res.data) {
+                    this.setState({
+                        editedEntry: {}
+                    })
+                    //Redirect to entry page for now
+                    this.props.history.push('/entry');
+                } else {
+                    //If sign in fails, stay on login
+                    this.props.history.push('/');
+                }
+            }).catch(err => console.log(err));
 
-        // }
+        }
 
         // This method handles entry deletion
         this.handleDeleteEntry= (entry_id) => {
