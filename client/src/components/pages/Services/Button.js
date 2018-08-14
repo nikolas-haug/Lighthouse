1b961c67c7a0f19b9aa04f95bf5035d25e450886
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-14 01:16:52 -0500

    services page

--- client/src/components/pages/Services/Button.js
+++ client/src/components/pages/Services/Button.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./services.css";
 
 const Button = (props) => {
   return(
-    <button className="btn btn-primary btn-lg btn-search">Search Providers</button>
+    <button className="btn btn-lg btn-search">Search Providers</button>
   )
 };
 
