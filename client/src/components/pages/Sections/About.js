b35e86111352a320bf4363508d3e5320e794a4a5
Eric Baltutis <34050626+baltu008@users.noreply.github.com>
2018-08-13 22:07:32 -0500

    about statement

--- client/src/components/pages/Sections/About.js
+++ client/src/components/pages/Sections/About.js
@@ -5,10 +5,14 @@ import '../Landing/Login/login.css'
 const About = () => {
     return (
         <div className="about-block">
-        <h3>About LightHouse</h3>
-        <p>Lighthouse is a journaling app tailored to supporting people with mental health issues. Track you progress in your personal journal and head over to the forums to interact with others in similar situation to yourself</p>
-    </div>
-        
+            <h3>About LightHouse</h3>
+            <p>At Lighthouse, we are here to be a beacon of hope for those struggling with mental health issues. We want you to
+               not only find professional help, but also to open your voice to the community and find comfort and support
+              from others that may be struggling in similar ways. Remember, "There is no health without mental health; mental
+              health is too important to bbe left to the professionals alone, and mental health is everyone's business." ~Vikram
+          Patel</p>
+        </div>
+
     );
 }
 
