58d5b35bfad2e8a033f2c0691f13491e6f41ad28
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-14 19:10:58 -0500

    changes

---
+++ client/src/components/pages/Sections/LikeButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "../Entry/Comments/comments.css";
+
+
+const LikeButton = (props) => {
+    console.log(props)
+    return (
+        <button 
+        data-id={props.comment._id} 
+        action="like"
+        onClick={props.handleAction} 
+        type="button" 
+        className="btn-link">
+        <span>{props.comment.likes.length?props.comment.likes.length:''} </span>
+        <b className="fa fa-thumbs-up" aria-hidden="true"></b>
+       Like</button>
+        
+    );
+}
+
+export default LikeButton;
