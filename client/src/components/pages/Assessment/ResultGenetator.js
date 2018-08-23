f502e813b42a69152abafe0c690f28539789f7ae
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-23 01:24:07 -0500

    date added

--- client/src/components/pages/Assessment/ResultGenetator.js
+++ client/src/components/pages/Assessment/ResultGenetator.js
@@ -15,6 +15,7 @@ export default {
         switch (true) {
             case score >= 20:
                 result = {
+                    date:Date.now(),
                     score: score,
                     PD: 'Major Depression (severe)',
                     TR: 'You should definitely seek help from a clinician. For the level of severity indicated by your responses, you should consider enlisting in regular counseling as well as an antidepressant regimen.'
@@ -22,6 +23,7 @@ export default {
                 break;
             case score >= 15 && score <= 19:
                 result = {
+                    date:Date.now(),
                     score: score,
                     PD: 'Major Depression (moderately severe)',
                     TR: 'While you might consider anti-depressants, you might benefit from regular psychotherapy alone. In any case, you should get in touch with a clinician to talk about your symptoms.'
@@ -29,6 +31,7 @@ export default {
                 break;
             case score >= 10 && score <= 14:
                 result = {
+                    date:Date.now(),
                     score: score,
                     PD: 'Minor Depression',
                     TR: 'It sounds like you might just be in a bit of a funk. Keep track of how you are feeling in our journaling app. If your do not improve within a month, consider a lifestyle change or seeking counseling.'
@@ -36,6 +39,7 @@ export default {
                 break;
             case score >= 5 && score <= 9:
                 result = {
+                    date:Date.now(),
                     score: score,
                     PD: 'Minimal Symptoms of Depression',
                     TR: 'It sounds like the shades of blue that you are experiencing are probably part of a robust and healthy emotional life. Start journaling to probe the contours of your score-to-score emotional life. Post your entries to our forums - sometimes conversation is the best medicine!'
@@ -43,6 +47,7 @@ export default {
                 break;
             case score < 5:
                 result = {
+                    date:Date.now(),
                     score: score,
                     PD: 'No Symptoms',
                     TR: 'Wow! You sound like you are really enjoying life. Consider writing a journal entry to reflect on how you have gotten to this zen place. Maybe even share it to the forums - you may just inspire someone!'
