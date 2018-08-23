898405592a72baba00cc9adac90ebc9883033551
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-22 19:44:08 -0500

    move assessment title into a componemt

---
+++ client/src/components/pages/Assessment/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = (props) => {
+    return (
+        <div>
+             <h2 className="assessement-title">{props.title}</h2>
+        </div>
+    );
+}
+
+export default Title;
+
+
