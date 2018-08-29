2bb371eb85d05c00c984a2cb3dd1533ca2026d97
Emerson Doyah <emerson.doyah@gmail.com>
2018-08-28 19:04:13 -0500

    middleware feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import Entry from './components/pages/Entry/Entry'
 import Wrapper from './components/Wrapper/Wrapper'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Services from "./components/pages/Services/Services";
 import Landing from './components/pages/Landing/Landing';
-import Journal from './components/pages/Journal/Journal';
-import EditEntry from './components/pages/Entry/Edit_entry';
-import './sticky.css';
-
 import Footer from './components/Footer/Footer';
 import SearchResults from './components/Footer/SearchResults';
-import Assessment from './components/pages/Assessment/Assessment';
+import Middleware from './components/Middleware';
+import './sticky.css';
+
+
 
 const App = () => (
   
@@ -20,13 +18,13 @@ const App = () => (
         <Route exact path="/" component={Landing} />
         <Route exact path="/signup" component={Landing} />
         <Route exact path="/logout" component={Landing} />
-        <Route exact path="/entries" component={Entry} />
-        <Route exact path="/new_entry" component={Entry} />
-        <Route exact path="/edit_entry/:id" component={EditEntry} />
-        <Route exact path="/my_journals" component={Journal} />
+        <Route exact path="/entries" component={Middleware} />
+        <Route exact path="/new_entry" component={Middleware} />
+        <Route exact path="/edit_entry/:id" component={Middleware} />
+        <Route exact path="/my_journals" component={Middleware} />
         <Route exact path="/services" component={Services} />
         <Route exact path="/custom_search/:term" component={SearchResults}/>
-        <Route exact path="/assessment" component={Assessment}/>
+        <Route exact path="/assessment" component={Middleware}/>
         <div className="push"></div>
       </div>
       <div className="footer">
